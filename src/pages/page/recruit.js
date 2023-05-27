caa02e72270f6526221f2cc4c09d9085281ab41f
maitoyama <maitoyama@example.com>
2023-05-28 02:59:01 +0900

    commit

--- src/pages/page/recruit.js
+++ src/pages/page/recruit.js
@@ -1,6 +1,7 @@
 import { BreadCrumb } from "@/components/common/BreadCrumb/BreadCrumb";
 import Environment from "@/components/RecruitPage/Environment/Environment";
 import LetsEntry from "@/components/RecruitPage/LetsEntry/LetsEntry";
+import { OpenPosition } from "@/components/RecruitPage/OpenPosition/OpenPosition";
 import RecruitMain from "@/components/RecruitPage/RecruitMain/RecruitMain";
 import Layout from '../../components/common/Layout'
 import SelectionFlow from "../../components/RecruitPage/SelectionFlow/SelectionFlow";
@@ -10,7 +11,7 @@ export default function recruit(){
         <Layout>
             <BreadCrumb />
         <RecruitMain />
-
+        <OpenPosition />
         <Environment />
         <SelectionFlow />
         <LetsEntry />
