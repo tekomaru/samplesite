875b0180744797f12fde70260439c198db263f83
maitoyama <maitoyama@example.com>
2023-06-27 08:22:22 +0900

    変更

--- src/components/ServicePage/ServiceWeOffer/ServiceWeOffer.jsx
+++ src/components/ServicePage/ServiceWeOffer/ServiceWeOffer.jsx
@@ -11,19 +11,19 @@ export default function ServiceWeOffer() {
       });
     return (
 
-        <div className={styles.ServiceWeOffer_wrapper__tN43d} ref={ref}>
+        <div className={styles.ServiceWeOffer_wrapper} ref={ref}>
             {inView && (
-            <div className={styles.Headline_headline__tn7LE}>
+            <div className={styles.Headline_headline}>
                 <div className={styles.Headline_mainHeadline__GKrWA}>Service we offer</div>
                     <span>提供サービス</span>
                 </div>
             )}
                 {inView &&(
-                <div className={styles.ServiceWeOffer_serviceWeOffers__eQUrZ}>
-                    <div className={`${styles.ServiceWeOffer_offerContent__q4vdh} animate__animated animate__fadeInUp`}>
+                <div className={styles.ServiceWeOffer_serviceWeOffers}>
+                    <div className={`${styles.ServiceWeOffer_offerContent} animate__animated animate__fadeInUp`}>
                         <p>エンジニアリング支援</p>
                         <p>Engineering Support</p>
-                        <div className={styles.ServiceWeOffer_offerContentBox__iBCYe}>
+                        <div className={styles.ServiceWeOffer_offerContentBox}>
                             <p>システムの構築に関する コンサルティング、設計、開発、運用を行います。<br/>
                             <br />
                             <br />
@@ -32,20 +32,20 @@ export default function ServiceWeOffer() {
                             {/* <Link href="/service/ServiceSystemDevelopment/">VIEW MORE</Link> */}
                         </div>
                     </div>
-                    <div className={`${styles.ServiceWeOffer_offerContent__q4vdh} animate__animated animate__fadeInUp`} id="ServiceWeOffer_offerContent02__kLg_g">
+                    <div className={`${styles.ServiceWeOffer_offerContent} animate__animated animate__fadeInUp`} id="ServiceWeOffer_offerContent02__kLg_g">
                         <p>Web広告運用支援</p>
                         <p>Web advertising Support</p>
-                        <div className={styles.ServiceWeOffer_offerContentBox__iBCYe}>
+                        <div className={styles.ServiceWeOffer_offerContentBox}>
                             <p>デジタル広告の領域での専門知識と経験を活かし、
                                 クライアントの目標に合わせたカスタマイズされた戦略を策定し、 効果的な広告キャンペーンの設計と最適化を支援します。</p>
                             {/* <Link href="/page/ServiceAppDevelopment/">VIEW MORE</Link> */}
                         </div>
                     </div>
-                    <div className={`${styles.ServiceWeOffer_offerContent__q4vdh} animate__animated animate__fadeInUp`} id="ServiceWeOffer_offerContent03__vTnju">
+                    <div className={`${styles.ServiceWeOffer_offerContent} animate__animated animate__fadeInUp`} id="ServiceWeOffer_offerContent03__vTnju">
                         <p>自社サービス企画・開発</p>
                         <p>Own Service</p>
                         
-                        <div className={styles.ServiceWeOffer_offerContentBox__iBCYe}>
+                        <div className={styles.ServiceWeOffer_offerContentBox}>
                             <p>社内メンバーで自由にアイデアを出し合い、プロジェクトを立ち上げて事業化を目指します。現在もメンバーから挙がったアイデアを具体化しプロジェクトを立ち上げて開発を進めています。</p>
                             {/* <Link href="/service/ServiceOwnService/">VIEW MORE</Link> */}
                         </div>
