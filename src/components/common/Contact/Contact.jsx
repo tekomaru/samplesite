c833da71995442b509102b7d13e11e98e87a8562
maitoyama <maitoyama@example.com>
2023-06-27 02:00:53 +0900

    修正

--- src/components/common/Contact/Contact.jsx
+++ src/components/common/Contact/Contact.jsx
@@ -3,20 +3,20 @@ import styles from './Contact.module.css'
 
 export default function Contact() {
     return (
-        <div className={styles.Contact_wrapper__F1Fby}>
+        <div className={styles.Contact_wrapper}>
             <div className={`${styles.animate__animated} animate__fadeInUp}`}>
-                <div className={styles.Contact_contactContent__5LDWC}>
-                    <div className={styles.Contact_contactLeftContent__WJ1dY}>
-                        <div className={styles.Contact_contactTitle__fuoH7}>
+                <div className={styles.Contact_contactContent}>
+                    <div className={styles.Contact_contactLeftContent}>
+                        <div className={styles.Contact_contactTitle}>
                             <div>Let's Have a</div>
                             <div>Conversation!</div>
                         </div>
-                        <div className={styles.Contact_companyIntroduction__ygv1K}>
+                        <div className={styles.Contact_companyIntroduction}>
                             <p>Webサービスやアプリ開発のご相談、協業パートナーなどその他のお問い合わせはこちらから。</p>
                         </div>
                     </div>
                     <div>
-                        <Link className={styles.Contact_contactCircle__0vuZB} href="/CONTACT/">
+                        <Link className={styles.Contact_contactCircle} href="/CONTACT/">
                             <span>CONTACT</span>
                         </Link>
                     </div>
